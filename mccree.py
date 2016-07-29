bf5583245bd5f2f3a0bf130ddc271e2963a4f8e1
shankera <rjnshanker@gmail.com>
2016-07-29 00:32:54 -0400

    combined if statements, mccree does not respond to himself

--- mccree.py
+++ mccree.py
@@ -1,5 +1,7 @@
 import os
 import time
+import string
+
 from slackclient import SlackClient
 # starterbot's ID as an environment variable
 BOT_ID = os.environ.get("MCCREE_ID")
@@ -7,37 +9,32 @@ BOT_ID = os.environ.get("MCCREE_ID")
 # constants
 AT_BOT = "<@" + BOT_ID + ">:"
 THE_QUESTION = "what time is it"
+HIGH_NOON = "High Noon"
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('MCCREE_BOT_TOKEN'))
 
 
 def handle_command(command, channel):
-    if THE_QUESTION in command:
+    if THE_QUESTION in command or "what time it is" in command :
         response = "It's High Noon. . ."
         slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
-    if "what time it is" in command :
-        response = "It's High Noon. . ."
+    elif HIGH_NOON.lower() in command:
+        response = "You're damn right"
         slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
-    if "when" in command:
-        response = "High Noon"
+    elif command.startswith(("what time")) or string.replace(command, "whenever", "").startswith(("when")):
         slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
-
+                          text=HIGH_NOON, as_user=True)
 
 def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output:
-                # return text after the @ mention, whitespace removed
+                if output['user'] == BOT_ID:
+                    return None, None
                 return output['text'].lower(), \
                        output['channel']
     return None, None
