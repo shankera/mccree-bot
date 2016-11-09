a7d080480a164907cdce2fac5ecc36c494a675b0
Arjun Shanker <arjunshanker@twice.attlocal.net>
2016-11-09 09:20:11 -0500

    readded version1, updated readme

---
+++ Version1/mccree.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# all of the junk to run mccree bot
+
+virtualenv mccree
+source mccree/bin/activate
+pip install slackclient
+for key in `cat .botkeys`; do
+    export MCCREE_BOT_TOKEN=$key
+    export MCCREE_ID=$(python print_bot_id.py 2>&1)
+    nohup python mccree.py &
+done
